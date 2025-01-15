a43d12a855c1740aec992704788c037438512781
Serhii_Radkovskyi <serhii.radkovskyi@tech11.com>
2025-01-15 16:04:27 +0100

    update path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
